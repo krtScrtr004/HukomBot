632553db02381c2f9cd884eaefda4fa1cbc2f64a
Kurt <secretariokurt3219@gmail.com>
2026-06-25 20:35:43 +0800

    change: replace ChromaDB with Postgre with pgVector

--- src/database/database.py
+++ src/database/database.py
@@ -1,17 +1,25 @@
 import os
-import chromadb
-from chromadb import Collection
-from util.utility import get_project_root
+import psycopg
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 class Database:
-    COLLECTION_PATH = get_project_root() / "chroma"
-    COLLECTION_NAME = "collection"
-
     def __init__(self):
-        self._chroma_client = chromadb.PersistentClient(
-            path = self.COLLECTION_PATH
+        DB_HOST = os.getenv("DB_HOST")
+        DB_PORT = os.getenv("DB_PORT")
+        DB_NAME = os.getenv("DB_NAME")
+        DB_USER = os.getenv("DB_USER")
+        DB_PASSWORD = os.getenv("DB_PASSWORD")
+
+        if not DB_HOST or not DB_PORT or not DB_NAME or not DB_USER or not DB_PASSWORD:
+            raise RuntimeError("Required database configuration missing")
+
+        self.conn = psycopg.connect(
+            host=DB_HOST,
+            port=DB_PORT,
+            dbname=DB_NAME,
+            user=DB_USER,
+            password=DB_PASSWORD,
         )
-
-
-    def get_or_create_collection(self) -> Collection:
-        return self._chroma_client.get_or_create_collection(name = self.COLLECTION_NAME)
