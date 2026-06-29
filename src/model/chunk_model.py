f0bb6891149d30ea0bf5d24a06555a2e3a8ca3dc
Kurt <secretariokurt3219@gmail.com>
2026-06-29 22:36:57 +0800

    change: make document and chunk models pydantic

--- src/model/chunk_model.py
+++ src/model/chunk_model.py
@@ -1,254 +1,30 @@
 from __future__ import annotations
+from typing import Optional
+from pydantic import BaseModel, model_validator
 from uuid import UUID
-from typing import List, Dict, Optional
-from model.model import Model
-from model.document_model import DocumentModel
-from datetime import datetime
-from psycopg import errors
+from typing import List, Optional
 
-
-class ChunkModel(Model):
-    def __init__(
-        self,
-        id: UUID = None,
-        document_id: UUID = None,
-        chunk_number: int = None,
-        chunk_text: str = None,
-        section: str = None,
-        embedding: list = None,
-        document: DocumentModel = None,
-    ):
-        super().__init__()
-
-        self.id = id
-        self.document_id = document.id if document is not None else document_id
-        self.chunk_number = chunk_number
-        self.chunk_text = chunk_text
-        self.section = section
-        self.embedding = embedding
-
-        self.document = document  # Navigation prop
-
-    def create(self):
-        document_id = self.document.id if self.document is not None else self.document_id
-        if not document_id:
-            raise RuntimeError("Document Id not provided")
-        if not self.chunk_number:
-            raise RuntimeError("Chunk number not provided")
-        if not self.chunk_text:
-            raise RuntimeError("Chunk text not provided")
-        if not self.embedding:
-            raise RuntimeError("Embeddings not provided")
-        if not self.section:
-            raise RuntimeError("Section not provided")
-
-        with self.connection.connection() as conn:
-            try:
-                with conn.cursor() as cur:
-                    cur.execute(
-                        """
-                        INSERT INTO chunks (
-                            document_id, chunk_number, chunk_text, embedding, section
-                        ) VALUES (
-                            %s, %s, %s, %s, %s
-                        ) RETURNING id
-                        """,
-                        (
-                            document_id,
-                            self.chunk_number,
-                            self.chunk_text,
-                            self.embedding,
-                            self.section,
-                        ),
-                    )
-
-                    # Set chunk id
-                    self.id = cur.fetchone()[0]
-
-                conn.commit()
-            except errors.IntegrityError as ex:
-                print(f"Integrity error exception: {ex}")
-                conn.rollback()
-            except errors.ForeignKeyViolation as ex:
-                print(f"Document id not found exception: {ex}")
-                conn.rollback()
-            except errors.OperationalError as ex:
-                print(f"Insert error: {ex}")
-                raise
-
-    def search(self, chunk_text: str, limit: int = 10, offset: int = 0):
-        with self.connection.connection() as conn:
-            try:
-                with conn.cursor() as cur:
-                    cur.execute(
-                        """
-                        WITH query AS (
-                            SELECT plainto_tsquery('english', %s) AS q
-                        )
-                        SELECT
-                            c.id AS c_id,
-                            c.chunk_number AS c_chunk_number,
-                            c.chunk_text AS c_chunk_text,
-                            c.embedding AS c_embedding,
-                            c.section AS c_section,
-                            ts_rank(c.search_vector, q.q) AS c_rank,
-                            d.id AS d_id,
-                            d.title AS d_title,
-                            d.file_type AS d_file_type
-                        FROM chunks c
-                        JOIN documents d ON c.document_id = d.id
-                        CROSS JOIN query q
-                        WHERE c.search_vector @@ q.q
-                        ORDER BY c_rank DESC
-                        LIMIT %s
-                        OFFSET %s
-                        """,
-                        (chunk_text, limit, offset),
-                    )
-
-                    rows = cur.fetchall()
-
-                    chunks = []
-                    for row in rows:
-                        chunk = ChunkModel(
-                            id=UUID(row["c_id"]),
-                            document_id=UUID(row["d_id"]),
-                            chunk_number=int(row["c_chunk_number"]),
-                            chunk_text=row["c_chunk_text"],
-                            embedding=row["c_embedding"],
-                            section=row["c_section"],
-                            document=DocumentModel(
-                                id=row["d_id"],
-                                title=row["d_title"],
-                                file_type=row["d_file_type"],
-                            ),
-                        )
-                        chunks.append(chunk)
-
-                    return chunks
-            except errors.OperationalError as ex:
-                print(f"Insert error: {ex}")
-                raise
-
-    def search_vector(self, chunk_embedding: list, limit: int = 10, offset: int = 0):
-        with self.connection.connection() as conn:
-            try:
-                with conn.cursor() as cur:
-                    cur.execute(
-                        """
-                        SELECT 
-                            c.id as c_id,
-                            c.chunk_number as c_chunk_number,
-                            c.chunk_text as c_chunk_text,
-                            c.embedding as c_embedding,
-                            c.section as c_section,
-                            d.id as d_id,
-                            d.title as d_title,
-                            d.file_type as d_file_type
-                        FROM chunks c
-                        JOIN documents d
-                            ON c.document_id = d.id
-                        ORDER BY c.embedding <=> %s::vector
-                        LIMIT %s
-                        OFFSET %s
-                        """,
-                        (str(chunk_embedding), limit, offset),
-                    )
-
-                    rows = cur.fetchall()
-
-                    chunks = []
-                    for row in rows:
-                        chunk = ChunkModel(
-                            id=row["c_id"],
-                            document_id=row["d_id"],
-                            chunk_number=int(row["c_chunk_number"]),
-                            chunk_text=row["c_chunk_text"],
-                            embedding=row["c_embedding"],
-                            section=row["c_section"],
-                            document=DocumentModel(
-                                id=row["d_id"],
-                                title=row["d_title"],
-                                file_type=row["d_file_type"],
-                            ),
-                        )
-                        chunks.append(chunk)
-
-                    return chunks
-            except errors.OperationalError as ex:
-                print(f"Insert error: {ex}")
-                raise
-
-    @staticmethod
-    def create_many(chunks: List[ChunkModel]):
-        if not chunks:
-            return
-
-        model = ChunkModel()
-
-        params = []
-        for chunk in chunks:
-            entry = {}
-            document_id = chunk.document.id if chunk.document is not None else chunk.document_id
-            if not document_id:
-                raise RuntimeError("Document Id not provided")
-            entry["document_id"] = document_id
-            if chunk.chunk_number < 0:
-                raise RuntimeError("Chunk number not provided")
-            entry["chunk_number"] = chunk.chunk_number
-            if not chunk.chunk_text:
-                raise RuntimeError("Chunk text not provided")
-            entry["chunk_text"] = chunk.chunk_text
-            if not all(chunk.embedding):
-                raise RuntimeError("Embeddings not provided")
-            entry["embedding"] = chunk.embedding
-            if not chunk.section:
-                raise RuntimeError("Section not provided")
-            entry["section"] = chunk.section
-            params.append(entry)
-
-        with model.connection.connection() as conn:
-            try:
-                with conn.cursor() as cur:
-                    cur.executemany(
-                        """
-                        INSERT INTO chunks (
-                            document_id, chunk_number, chunk_text, embedding, section
-                        ) VALUES (
-                            %s, %s, %s, %s, %s
-                        ) RETURNING id
-                        """,
-                        [
-                            (
-                                param["document_id"],
-                                param["chunk_number"],
-                                param["chunk_text"],
-                                param["embedding"],
-                                param["section"],
-                            )
-                            for param in params
-                        ],
-                        returning=True,
-                    )
-
-                    # Retrieve the generated IDs
-                    counter = 0
-                    while True:
-                        row = cur.fetchone()
-                        if row:
-                            # Update the chunk's ID attribute
-                            chunks[counter].id = row[0]
-                            counter += 1
-                        if not cur.nextset():
-                            break
-
-                conn.commit()
-            except errors.IntegrityError as ex:
-                print(f"Integrity error exception: {ex}")
-                conn.rollback()
-            except errors.ForeignKeyViolation as ex:
-                print(f"Document id not found exception: {ex}")
-                conn.rollback()
-            except errors.OperationalError as ex:
-                print(f"Insert error: {ex}")
-                raise
+class Chunk(BaseModel):
+    id: UUID
+    document_id: UUID
+    chunk_number: int
+    chunk_text: str
+    section: Optional[str] = None
+    embedding: list
+    
+class ChunkCreate(Chunk):
+    document_id: UUID
+    chunk_number: int
+    chunk_text: str
+    section: Optional[str] = None
+    embedding: list
+    
+class ChunkSearchKeyword(Chunk):
+    chunk_text: str
+    limit: int = 10
+    offset: int = 0
+    
+class ChunkSearchVector(Chunk):
+    embeddings: List
+    limit: int = 10
+    offset: int = 0
--- src/model/document_model.py
+++ src/model/document_model.py
@@ -1,187 +1,31 @@
 from __future__ import annotations
+from pydantic import BaseModel, Field, model_validator
 from uuid import UUID
-from typing import List, Dict, Optional
-from model.model import Model
+from typing import Optional
 from datetime import datetime
-from psycopg import errors
 
 
-class DocumentModel(Model):
-    def __init__(
-        self,
-        id: UUID = None,
-        title: str = None,
-        file_type: str | None = None,
-        created_at: datetime | None = None,
-    ):
-        super().__init__()
+class Document(BaseModel):
+    id: UUID
+    title: str
+    file_type: Optional[str] = None
+    created_at: datetime
 
-        self.id = id
-        self.title = title
-        self.file_type = file_type
-        self.created_at = created_at
 
-    def create(self):
-        if not self.title:
-            raise RuntimeError("Title not provided")
+class DocumentCreate(Document):
+    title: str
+    file_type: Optional[str] = None
+    created_at: datetime = Field(default_factory=datetime.now)
 
-        with self.connection.connection() as conn:
-            try:
-                file_type_c = self.file_type if self.file_type else None
-                created_at_c = self.created_at if self.created_at else datetime.now()
 
-                with conn.cursor() as cur:
-                    cur.execute(
-                        """
-                        INSERT INTO documents (
-                            title, file_type, created_at
-                        ) VALUES (
-                            %s, %s, %s
-                        ) ON CONFLICT (title)
-                        DO UPDATE SET
-                            file_type = EXCLUDED.file_type
-                        RETURNING id
-                        """,
-                        (self.title, file_type_c, created_at_c),
-                    )
+class DocumentSearch(Document):
+    title: Optional[str] = None
+    file_type: Optional[str] = None
+    limit: int = 10
+    offset: int = 0
 
-                    # Set document id
-                    self.id = cur.fetchone()[0]
-
-                conn.commit()
-            except errors.IntegrityError as ex:
-                print(f"Integrity error exception: {ex}")
-                conn.rollback()
-            except errors.OperationalError as ex:
-                print(f"Insert error: {ex}")
-                raise
-
-    def search(self, title: str = "", file_type: str = None) -> list:
-        if not title and not file_type:
-            raise RuntimeError("Title OR file_type parameters must be provided")
-
-        with self.connection.connection() as conn:
-            try:
-                combined_terms = ""
-                if title:
-                    combined_terms += f" {title}"
-                if file_type:
-                    combined_terms += f" {file_type}"
-
-                with conn.cursor() as cur:
-                    cur.execute(
-                        """
-                        WITH query AS (
-                            SELECT plainto_tsquery('english', %s) AS q
-                        )
-                        SELECT
-                            d.*,
-                            ts_rank(d.search_vector, q.q) AS rank
-                        FROM documents d, query q
-                        WHERE d.search_vector @@ q.q
-                        ORDER BY rank DESC
-                        """,
-                        (combined_terms,),
-                    )
-
-                    rows = cur.fetchall()
-
-                    documents = []
-                    for row in rows:
-                        document = DocumentModel(
-                            id=row["id"],
-                            title=row["title"],
-                            file_type=row["file_type"],
-                            created_at=datetime.fromisoformat(row["created_at"]),
-                        )
-                        documents.append(document)
-
-                    return documents
-            except errors.OperationalError as ex:
-                print(f"Search error: {ex}")
-                raise
-
-    @staticmethod
-    def create_many(documents: List[DocumentModel]):
-        # Return if list is empty
-        if not documents:
-            return
-
-        model = DocumentModel()
-
-        params = []
-        for doc in documents:
-            entry = {}
-            if not doc.title:
-                raise RuntimeError("Title not provided")
-            entry["title"] = doc.title
-            # Set default to None or current time if not provided
-            entry["file_type"] = doc.file_type if doc.file_type else None
-            entry["created_at"] = doc.created_at if doc.created_at else datetime.now()
-            params.append(entry)
-
-        with model.connection.connection() as conn:
-            try:
-                with conn.cursor() as cur:
-                    cur.executemany(
-                        """
-                        INSERT INTO documents (
-                            title, file_type, created_at
-                        ) VALUES (
-                            %s, %s, %s
-                        ) ON DUPLICATE SET
-                            file_type = EXCULDED.file_type
-                        RETURNING id
-                        """,
-                        [
-                            (param["title"], param["file_type"], param["created_at"])
-                            for param in params
-                        ],
-                        returning=True,
-                    )
-                    # Retrieve the generated IDs
-                    counter = 0
-                    while True:
-                        row = cur.fetchone()
-                        if row:
-                            # Update the document's ID attribute
-                            documents[counter].id = row[0]
-                            counter += 1
-                        if not cur.nextset():
-                            break
-
-                conn.commit()
-            except errors.IntegrityError as ex:
-                print(f"Integrity error exception: {ex}")
-                conn.rollback()
-            except errors.OperationalError as ex:
-                print(f"Insert error: {ex}")
-                raise
-
-    @staticmethod
-    def delete_many(ids: list[UUID]):
-        if not ids:
-            return
-
-        model = DocumentModel()
-
-        with model.connection.connection() as conn:
-            try:
-                with conn.cursor() as cur:
-                    placeholders = ", ".join(["%s"] * len(ids))
-                    with conn.cursor() as cur:
-                        cur.execute(
-                            f"""
-                            DELETE FROM documents
-                            WHERE id IN ({placeholders})
-                            """,
-                            ids,
-                        )
-
-                conn.commit()
-            except errors.IntegrityError as ex:
-                print(f"Integrity error exception: {ex}")
-                conn.rollback()
-            except errors.OperationalError as ex:
-                print(f"Insert error: {ex}")
-                raise
+    @model_validator(mode="after")
+    def at_least_one_required(self) -> DocumentSearch:
+        if self.title is None and self.file_type is None:
+            raise ValueError("At least one of 'title' or 'file_type' must be provided")
+        return self
