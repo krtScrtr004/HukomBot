d2af9b9836e36bf1a373c8c9c91beaf58c3ee1e0
Kurt <secretariokurt3219@gmail.com>
2026-07-01 22:52:24 +0800

    change: add optional status code InvalidDocumentTypeException

--- src/backend/app/exception/document_exception.py
+++ src/backend/app/exception/document_exception.py
@@ -1,5 +1,7 @@
 class InvalidDocumentTypeException(Exception):
-    def __init__(self, message: str = "Invalid file type"):
+    def __init__(self, message: str = "Invalid file type", status_code: int = None):
         self.message = message
+        self.status_code = status_code
+        
         super().__init__(self.message)
     
