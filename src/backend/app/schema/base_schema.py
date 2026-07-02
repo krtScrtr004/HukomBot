752f56c10af9c657e9f5e0f5f73026fb28c7edc6
Kurt <secretariokurt3219@gmail.com>
2026-07-02 21:48:49 +0800

    change: rename Response schema to BaseSchema

--- src/backend/app/schema/base_schema.py
+++ src/backend/app/schema/base_schema.py
@@ -1,7 +1,7 @@
 from typing import Any, List, Optional
 from pydantic import BaseModel, Field
 
-class Response(BaseModel):
+class BaseResponse(BaseModel):
     message: List[str] = Field(default_factory=list)
     data: Optional[Any] = None
     error: List[str] = Field(default_factory=list)
