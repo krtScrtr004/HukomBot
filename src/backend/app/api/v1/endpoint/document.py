872d022685afae87cdbb7cf6adfe8afdd2270d5a
Kurt <secretariokurt3219@gmail.com>
2026-07-03 21:40:15 +0800

    change: move out dependency.py for v1 api to src/backend/api/v1

---
+++ src/backend/app/api/v1/endpoint/document.py
@@ -0,0 +1,24 @@
+from fastapi import APIRouter, UploadFile, Depends, BackgroundTasks, File
+
+from backend.app.enum.upload_status import UploadStatus
+from backend.app.schema.document_schema import DocumentUploadResponse
+from backend.app.service.document_service import DocumentService
+
+from backend.app.api.v1.dependency import get_document_service
+
+document_router = APIRouter()
+
+@document_router.post("/")
+async def upload_document(
+    *,
+    file: UploadFile = File(...),
+    background_tasks: BackgroundTasks,
+    service: DocumentService = Depends(get_document_service)
+):
+    contents = await file.read()
+    
+    document = await service.create_pending_document(file.filename, contents)
+    background_tasks.add_task(
+        service.process_document_pdf_upload, document.id, file.filename, contents
+    )
+    return DocumentUploadResponse(message=["File is uploading"], document_id=document.id, status=UploadStatus.ONGOING)
