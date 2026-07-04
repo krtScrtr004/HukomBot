bda3621fc20dcb75922ce4d5e23aefd8dfebb690
Kurt <secretariokurt3219@gmail.com>
2026-07-04 13:20:40 +0800

    change: add vallidation to Document model attributes

--- src/backend/app/model/document_model.py
+++ src/backend/app/model/document_model.py
@@ -8,10 +8,10 @@ from backend.app.enum.upload_status import UploadStatus
 
 class Document(BaseModel):
     id: UUID
-    title: str
-    file_type: Optional[str] = None
+    title: str = Field(min_length=1, max_length=300)
+    file_type: Optional[str] = Field(default=None, min_length=1, max_length=20)
     upload_status: UploadStatus = Field(default=UploadStatus.PENDING)
-    upload_error: str = Field(default=None)
-    created_at: datetime
+    upload_error: Optional[str] = Field(default=None, max_length=500)
+    created_at: datetime = Field(default_factory=datetime.now)
     
     model_config = {"arbitrary_types_allowed": True}
--- src/backend/app/schema/document_schema.py
+++ src/backend/app/schema/document_schema.py
@@ -18,7 +18,7 @@ class DocumentCreate(BaseModel):
     title: str = Field(min_length=1, max_length=300)
     file_type: Optional[str] = Field(default=None, min_length=1, max_length=20)
     upload_status: UploadStatus = Field(default=UploadStatus.PENDING)
-    upload_error: str = Field(default=None, max_length=500)
+    upload_error: Optional[str] = Field(default=None, max_length=500)
     created_at: datetime = Field(default_factory=datetime.now)
 
     model_config = {"from_attributes": True}
