d92e884594261ab56ebc6520cb31aa85e143a99e
Kurt <secretariokurt3219@gmail.com>
2026-07-09 15:42:06 +0800

    change: add more exception handlers in main.py

--- src/backend/app/main.py
+++ src/backend/app/main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI, Request, HTTPException
+from psycopg import errors
+from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
 
 from backend.app.api.v1.endpoint.document import document_api_router
@@ -15,15 +16,35 @@ setup_logging()
 app = FastAPI(lifespan=lifespan)
 
 # Routers
-app.include_router(router=document_api_router, prefix="/api/documents", tags=["Documents API"])
+app.include_router(
+    router=document_api_router, prefix="/api/documents", tags=["Documents API"]
+)
 
+# Exception Handlers
 
-@app.exception_handler(HTTPException)
-async def http_exception(equest: Request, exc: ChunkFileException):
+async def handle_database_exception(request: Request, exc: Exception):
     return JSONResponse(
-        status_code=exc.status_code,
-        content=BaseResponse(error=[exc.message]).model_dump(),
+        status_code=500,
+        content=BaseResponse(error=[f"Database error: {str(exc)}"]).model_dump(),
     )
+
+
+@app.exception_handler(Exception)
+async def http_exception(request: Request, exc: Exception):
+    return JSONResponse(
+        status_code=500,
+        content=BaseResponse(error=[str(exc)]).model_dump(),
+    )
+
+
+db_exceptions = [
+    errors.IntegrityError,
+    errors.ForeignKeyViolation,
+    errors.OperationalError,
+]
+for db_exec in db_exceptions:
+    app.add_exception_handler(db_exec, handle_database_exception)
+
 
 @app.exception_handler(ChunkFileException)
 async def chunk_file_exception(request: Request, exc: ChunkFileException):
