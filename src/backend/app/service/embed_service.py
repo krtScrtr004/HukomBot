373b4c07024bb6c8f269ba5f75712acb233f5949
Kurt <secretariokurt3219@gmail.com>
2026-07-11 22:47:02 +0800

    change: use singleton pattern for EmbedService and RerankService

--- src/backend/app/service/embed_service.py
+++ src/backend/app/service/embed_service.py
@@ -1,12 +1,28 @@
+from __future__ import annotations
+
 import numpy as np
 from sentence_transformers import SentenceTransformer
 
 
 class EmbedService:
     MODEL = "BAAI/bge-base-en-v1.5"
+    
+    __instance = EmbedService|None = None
 
     def __init__(self):
         self.__model = SentenceTransformer(model_name_or_path=EmbedService.MODEL)
+        
+    @classmethod
+    def initialize(cls) -> EmbedService:
+        if cls.__instance is None:
+            cls.__instance = cls()
+        return cls.__instance
+    
+    @classmethod
+    def get_instance(cls) -> EmbedService:
+        if cls.__instance is None:
+            return RuntimeError("Embed service is not initialized")
+        return cls.__instance
 
     def embed_documents(self, documents: list[str]) -> list[list[float]]:
         embeddings = self.__model.encode(documents, normalize_embeddings=True)
--- src/backend/app/service/reranker_service.py
+++ src/backend/app/service/reranker_service.py
@@ -1,10 +1,24 @@
-from backend.app.model.chunk_model import Chunk
+from __future__ import annotations
 from sentence_transformers import CrossEncoder
+from backend.app.model.chunk_model import Chunk
 
 
 class RerankService:
+    __instance = RerankService|None = None
     def __init__(self):
         self.__model = CrossEncoder("BAAI/bge-reranker-v2-m3")
+        
+    @classmethod
+    def initialize(cls) -> RerankService:
+        if cls.__instance is None:
+            cls.__instance = cls()
+        return cls.__instance
+    
+    @classmethod
+    def get_instance(cls) -> RerankService:
+        if cls.__instance is None:
+            return RuntimeError("Rerank service is not initialized")
+        return cls.__instance
 
     def rerank(self, query: str, chunks: list[Chunk]):
         pairs = [(query, chunk.chunk_text) for chunk in chunks]
