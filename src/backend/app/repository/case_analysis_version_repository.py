315ca71c8d9b0a29573c4a7b0b78c93f742f7b5e
Kurt <secretariokurt3219@gmail.com>
2026-07-12 22:17:31 +0800

    change: rollback get_id_by_session_id in CaseAnalysisVersionRepository as it has no current use

--- src/backend/app/repository/case_analysis_version_repository.py
+++ src/backend/app/repository/case_analysis_version_repository.py
@@ -19,7 +19,7 @@ class CaseAnalysisVersionRepository:
     ) -> CaseAnalysisVersion:
         if connection is not None:
             return await self.__create_implement(connection, case_analysis_version)
-
+        
         async with self.__database.connection() as conn:
             try:
                 result = await self.__create_implement(conn, case_analysis_version)
@@ -72,7 +72,7 @@ class CaseAnalysisVersionRepository:
             return await self.__create_many_implement(
                 connection, case_analysis_versions
             )
-
+            
         async with self.__database.connection() as conn:
             try:
                 result = await self.__create_many_implement(
@@ -128,35 +128,6 @@ class CaseAnalysisVersionRepository:
             )
 
         return updated
-
-    async def get_id_by_session_id(
-        self, case_analysis_session_id: UUID
-    ) -> UUID|None:
-        async with self.__database.connection() as conn:
-            try:
-                async with conn.cursor() as cur:
-                    await cur.execute(
-                        """
-                        SELECT cav.id
-                        FROM case_analysis_versions cav
-                        INNER JOIN case_analysis_sessions cas
-                            ON cav.case_analysis_session_id = cas.id
-                        WHERE cas.id = %s
-                        ORDER BY cav.created_at DESC
-                        LIMIT 1
-                        """,
-                        (case_analysis_session_id,),
-                    )
-
-                    row = await cur.fetchone()
-                await conn.commit()
-                return (
-                    row["id"]
-                    if row is not None and row["id"] is not None
-                    else None
-                )
-            except (errors.OperationalError, errors.IntegrityConstraintViolation) as ex:
-                raise
 
     async def get_latest_analysis_version_by_session_id(
         self, case_analysis_session_id: UUID
