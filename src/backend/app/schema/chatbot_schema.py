7f79772a74850b8806c4c08ba55631f9a73a051d
Kurt <secretariokurt3219@gmail.com>
2026-07-12 15:39:16 +0800

    change: update CaseAnalysisCaseFactsPayload to have optional updated / deleted case fact lists

--- src/backend/app/schema/chatbot_schema.py
+++ src/backend/app/schema/chatbot_schema.py
@@ -17,6 +17,7 @@ class ChatPipelineResponse(BaseResponse):
     conversation_id: UUID = Field(default=None)
     answer: str
 
+    @classmethod
     @model_serializer(mode="wrap")
     def custom_serializer(self, handler: SerializerFunctionWrapHandler):
         result = handler(self)
@@ -37,12 +38,42 @@ CASE_FACT_MAX_LEN = 500
 
 class CaseAnalysisCaseFactsPayload(BaseModel):
     conversation_id: Optional[UUID] = Field(default=None)
-    case_facts: List[str] = Field(min_length=1, max_length=10)
+    new_case_facts: Optional[List[str]] = Field(
+        default_factory=[], min_length=1, max_length=10
+    )
+    updated_case_facts: Optional[List[Dict[UUID, str]]] = Field(
+        default_factory=[], min_length=1, max_length=10
+    )
+    deleted_case_facts: Optional[List[UUID]] = Field(
+        default_factory=[], min_length=1, max_length=10
+    )
+    
+    @classmethod
+    @model_validator(mode="after")
+    def atleast_one_list_required(self) -> CaseAnalysisCaseFactsPayload:
+        if not self.new_case_facts and not self.updated_case_facts and not self.deleted_case_facts:
+            return ValueError("At least one list must have value")
+        return self
+    
+    @classmethod
+    @model_validator(mode="after")
+    def has_conversation_id_update_delete_list(self):
+        if len(self.updated_case_facts) > 0 or len(self.deleted_case_facts) > 0 and not self.conversation_id:
+            return ValueError("Conversation id must be provided when editing / deleting a case fact")
+        return self
 
+    @classmethod
     @model_validator(mode="after")
     def is_allowed_case_fact(self) -> CaseAnalysisCaseFactsPayload:
-        for case_fact in self.case_facts:
+        for case_fact in self.new_case_facts:
             if len(case_fact) < CASE_FACT_MIN_LEN or len(case_fact) > CASE_FACT_MAX_LEN:
+                raise ValueError(
+                    f"Each case fact must be between {CASE_FACT_MIN_LEN} and {CASE_FACT_MAX_LEN} only"
+                )
+
+        for case_fact in self.updated_case_facts:
+            fact = next(iter(case_fact.values()))
+            if len(fact) < CASE_FACT_MIN_LEN or len(fact) > CASE_FACT_MAX_LEN:
                 raise ValueError(
                     f"Each case fact must be between {CASE_FACT_MIN_LEN} and {CASE_FACT_MAX_LEN} only"
                 )
@@ -55,6 +86,7 @@ class UpdateCaseFactsPayload(BaseModel):
 
     model_config = {"arbitrary_types_allowed": True}
 
+    @classmethod
     @model_validator(mode="after")
     def is_allowed_case_fact(self) -> UpdateCaseFactsPayload:
         for case_fact in self.case_facts:
