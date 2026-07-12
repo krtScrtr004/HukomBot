1afa90d5139847a307192d9ac67548a0e036f710
Kurt <secretariokurt3219@gmail.com>
2026-07-12 22:37:33 +0800

    fix: fix custom validation methods i CaseAnalysisCaseFactsPayload

--- src/backend/app/schema/chatbot_schema.py
+++ src/backend/app/schema/chatbot_schema.py
@@ -47,32 +47,44 @@ class CaseAnalysisCaseFactsPayload(BaseModel):
     deleted_case_facts: Optional[List[UUID]] = Field(
         default=None, min_length=1, max_length=10
     )
-    
+
     @model_validator(mode="after")
     def atleast_one_list_required(self) -> CaseAnalysisCaseFactsPayload:
-        if not self.new_case_facts and not self.updated_case_facts and not self.deleted_case_facts:
+        if (
+            not self.new_case_facts
+            and not self.updated_case_facts
+            and not self.deleted_case_facts
+        ):
             return ValueError("At least one list must have value")
         return self
-    
+
     @model_validator(mode="after")
     def has_conversation_id_update_delete_list(self):
-        if len(self.updated_case_facts) > 0 or len(self.deleted_case_facts) > 0 and not self.conversation_id:
-            return ValueError("Conversation id must be provided when editing / deleting a case fact")
+        if (
+            self.updated_case_facts
+            or self.deleted_case_facts
+            and not self.conversation_id
+        ):
+            return ValueError(
+                "Conversation id must be provided when editing / deleting a case fact"
+            )
         return self
 
     @model_validator(mode="after")
     def is_allowed_case_fact(self) -> CaseAnalysisCaseFactsPayload:
-        for case_fact in self.new_case_facts:
-            if len(case_fact) < CASE_FACT_MIN_LEN or len(case_fact) > CASE_FACT_MAX_LEN:
-                raise ValueError(
-                    f"Each case fact must be between {CASE_FACT_MIN_LEN} and {CASE_FACT_MAX_LEN} only"
-                )
+        if self.new_case_facts:
+            for case_fact in self.new_case_facts:
+                if len(case_fact) < CASE_FACT_MIN_LEN or len(case_fact) > CASE_FACT_MAX_LEN:
+                    raise ValueError(
+                        f"Each case fact must be between {CASE_FACT_MIN_LEN} and {CASE_FACT_MAX_LEN} only"
+                    )
 
-        for case_fact in self.updated_case_facts:
-            fact = next(iter(case_fact.values()))
-            if len(fact) < CASE_FACT_MIN_LEN or len(fact) > CASE_FACT_MAX_LEN:
-                raise ValueError(
-                    f"Each case fact must be between {CASE_FACT_MIN_LEN} and {CASE_FACT_MAX_LEN} only"
-                )
+        if self.updated_case_facts:
+            for case_fact in self.updated_case_facts:
+                fact = next(iter(case_fact.values()))
+                if len(fact) < CASE_FACT_MIN_LEN or len(fact) > CASE_FACT_MAX_LEN:
+                    raise ValueError(
+                        f"Each case fact must be between {CASE_FACT_MIN_LEN} and {CASE_FACT_MAX_LEN} only"
+                    )
 
         return self
