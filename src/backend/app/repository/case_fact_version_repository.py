890b6e259e43dec2e228d2416f836bd430616e85
Kurt <secretariokurt3219@gmail.com>
2026-07-12 22:18:33 +0800

    change: add update_many and get_latest_by_session_id methods in CaseFactVersionRepository

--- src/backend/app/repository/case_fact_version_repository.py
+++ src/backend/app/repository/case_fact_version_repository.py
@@ -1,10 +1,15 @@
+from uuid import UUID
 from typing import List
 from psycopg import errors
 from psycopg import AsyncConnection
 
 from backend.app.database.database import Database
 from backend.app.model.case_analysis_model import CaseFactVersion
-from backend.app.schema.case_analysis_schema import CaseFactVersionCreate
+from backend.app.schema.case_analysis_schema import (
+    CaseFactVersionCreate,
+    CaseFactVersionUpdate,
+    CaseFactVersionGetBySessionId,
+)
 
 
 class CaseFactVersionRepository:
@@ -126,3 +131,92 @@ class CaseFactVersionRepository:
                 )
 
         return updated
+
+    async def update_many(
+        self,
+        case_facts: List[CaseFactVersionUpdate],
+        connection: AsyncConnection = None,
+    ):
+        if not case_facts:
+            return
+
+        if connection is not None:
+            await self.__update_many_implement(connection, case_facts)
+        else:
+            async with self.__database.connection() as conn:
+                try:
+                    await self.__update_many_implement(conn, case_facts)
+                    await conn.commit()
+                except (errors.IntegrityError, errors.OperationalError) as ex:
+                    await conn.rollback()
+                    raise
+
+    async def __update_many_implement(
+        self, conn: AsyncConnection, case_facts: List[CaseFactVersionUpdate]
+    ):
+        query, params = self.__build_update_many_query(case_facts)
+
+        async with conn.cursor() as cur:
+            await cur.execute(query, params)
+
+    def __build_update_many_query(case_facts: List[CaseFactVersionUpdate]):
+        values_placeholder = []
+        values = {}
+        for i, case_fact in enumerate(case_facts):
+            d = case_fact.model_dump()
+            values_placeholder.append(
+                f"(%(id{i})s::uuid, %(version_number{i})s::int, %(fact{i})s::text, %(is_deleted{i})s::bool)"
+            )
+            values[f"id{i}"] = d["id"]
+            values[f"case_fact_id{i}"] = d["case_fact_id"]
+            values[f"version_number{i}"] = d["version_number"]
+            values[f"fact{i}"] = d["fact"]
+            values[f"is_deleted{i}"] = d["is_deleted"]
+
+        query = f"""
+            UPDATE case_fact_versions AS cfv
+            SET 
+                version_number = COALESCE(v.version_number, cfv.version_number),
+                fact = COALESCE(v.fact, cfv.fact),
+                is_deleted = COALESCE(v.is_deleted, cfv.is_deleted)
+            FROM (VALUES
+                {", ".join(values_placeholder)}
+            ) AS v(
+                id, 
+                version_number,
+                fact,
+                is_deleted
+            ) WHERE cfv.id = v.id
+        """
+
+        return query, values
+
+    async def get_latest_by_session_id(self, param: CaseFactVersionGetBySessionId):
+        async with self.__database.connection() as conn:
+            try:
+                async with conn.cursor() as cur:
+                    await cur.execute(
+                        """
+                        SELECT *
+                        FROM (
+                            SELECT DISTINCT ON (cf.id)
+                                cfv.*
+                            FROM case_facts cf
+                            JOIN case_fact_versions cfv
+                                ON cfv.case_fact_id = cf.id
+                            WHERE cf.case_analysis_session_id = %(case_analysis_session_id)s
+                            ORDER BY
+                                cf.id,
+                                cfv.version_number DESC
+                        ) latest
+                        WHERE latest.is_deleted = FALSE
+                        ORDER BY latest.created_at
+                        """,
+                        param.model_dump(),
+                    )
+
+                    row = await cur.fetchall()
+                await conn.commit()
+                return [CaseFactVersion.model_validate(case_fact) for case_fact in row]
+            except (errors.OperationalError, errors.IntegrityConstraintViolation) as ex:
+                raise
