6849496c76b80cdc1594c9a72cf01a1122444f15
Kurt <secretariokurt3219@gmail.com>
2026-07-19 22:56:46 +0800

    endpoint: add login callback for google login/registration

--- backend/app/api/v1/endpoint/auth.py
+++ backend/app/api/v1/endpoint/auth.py
@@ -1,14 +1,27 @@
 from typing import Annotated
-from fastapi import APIRouter, Request, Depends
-from backend.app.service.google_oauth_service import GoogleOAuthService
-from backend.app.api.v1.dependency import get_google_oauth_service
+from fastapi import APIRouter, Request, Query, Depends
+from backend.app.service.auth_service import AuthService
+from backend.app.service.google_service import GoogleService
+from backend.app.api.v1.dependency import get_auth_service, get_google_service
 
 auth_api_router = APIRouter()
 
 
 @auth_api_router.get("/google/login")
-async def google_login(
-    request: Request,
-    service: Annotated[GoogleOAuthService, Depends(get_google_oauth_service)],
+def google_login(
+    service: Annotated[GoogleService, Depends(get_google_service)],
 ):
     return service.redirect_to_authorization()
+
+
+@auth_api_router.get("/google/login/callback")
+async def google_login_callback(
+    code: Annotated[str, Query()],
+    auth_service: Annotated[AuthService, Depends(get_auth_service)],
+    google_service: Annotated[GoogleService, Depends(get_google_service)]
+): 
+    tokens = await google_service.request_tokens(code)
+    
+    google_user = await google_service.retrieve_user(tokens.id_token)
+    
+    return await auth_service.authenticate_google_user(google_user)
