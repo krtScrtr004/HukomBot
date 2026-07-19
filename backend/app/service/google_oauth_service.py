37acec2e739d9fb4f3d455cbbd7a4103ea085947
Kurt <secretariokurt3219@gmail.com>
2026-07-19 14:31:46 +0800

    change: add method in GoogleOAuthService to request for tokens in exchange for authorization code

--- backend/app/service/google_oauth_service.py
+++ backend/app/service/google_oauth_service.py
@@ -1,17 +1,37 @@
+import httpx
 from urllib.parse import urlencode
 from fastapi.responses import RedirectResponse
-from backend.app.core.settings import Settings
 
+from backend.app.core.settings import settings
+
+from backend.app.schema.auth_schema import GoogleOAuthTokenResponse
 
 class GoogleOAuthService:
     def redirect_to_authorization(self):
         params = {
-            "client_id": Settings.OAUTH_CLIENT_ID,
-            "redirect_uri": Settings.GOOGLE_OAUTH_REDIRECT_URI,
+            "client_id": settings.OAUTH_CLIENT_ID,
+            "redirect_uri": settings.GOOGLE_OAUTH_REDIRECT_URI,
             "response_type": "code",
             "scope": "openid email profile",
         }
-        url = urlencode(params)
-        
-        google_url = Settings.GOOGLE_AUTH_URL + "?" + urlencode(params)
-        return RedirectResponse(google_url)        
+
+        google_url = settings.GOOGLE_AUTH_URL + "?" + urlencode(params)
+        return RedirectResponse(google_url)
+
+    async def request_tokens(self, authorization_code: str):
+        async with httpx.AsyncClient() as client:
+            url = settings.GOOGLE_TOKEN_URL
+            body = {
+                "code": authorization_code,
+                "client_id": settings.OAUTH_CLIENT_ID,
+                "client_secret": settings.OAUTH_CLIENT_SECRET,
+                "redirect_uri": settings.GOOGLE_OAUTH_REDIRECT_URI,
+                "grant_type": "authorization_code",
+            }
+
+            response = await client.post(url=url, data=body)
+            if not response or response.status_code != 200:
+                raise Exception("An error occured while authentication")
+
+        token_response = GoogleOAuthTokenResponse.model_validate(response.json())
+        return token_response
