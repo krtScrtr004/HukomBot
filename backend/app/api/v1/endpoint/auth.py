8a838ea144f3ce821e65924c27dc8a32b4211760
Kurt <secretariokurt3219@gmail.com>
2026-07-20 17:00:20 +0800

    change: modify google login callback endpoint to redirect user instead of returning the jwt on json

--- backend/app/api/v1/endpoint/auth.py
+++ backend/app/api/v1/endpoint/auth.py
@@ -1,5 +1,11 @@
+import jwt
+
 from typing import Annotated
 from fastapi import APIRouter, Request, Query, Depends
+
+from fastapi.responses import RedirectResponse
+
+from backend.app.schema.auth_schema import JWTPayload
 
 from backend.app.service.auth_service import AuthService
 from backend.app.service.jwt_service import JWTService
@@ -8,7 +14,11 @@ from backend.app.service.google_service import GoogleService
 from backend.app.schema.auth_schema import LoginResponse
 from backend.app.schema.response_schema import SuccessResponse
 
-from backend.app.api.v1.dependency import get_auth_service, get_google_service
+from backend.app.api.v1.dependency import (
+    get_auth_service,
+    get_jwt_service,
+    get_google_service,
+)
 
 auth_api_router = APIRouter()
 
@@ -23,16 +33,25 @@ def google_login(
 @auth_api_router.get("/google/login/callback")
 async def google_login_callback(
     code: Annotated[str, Query()],
+    jwt_service: Annotated[JWTService, Depends(get_jwt_service)],
     auth_service: Annotated[AuthService, Depends(get_auth_service)],
     google_service: Annotated[GoogleService, Depends(get_google_service)],
 ):
-    tokens = await google_service.request_tokens(code)
+    try:
+        tokens = await google_service.request_tokens(code)
 
-    google_user = await google_service.retrieve_user(tokens.id_token)
+        google_user = await google_service.retrieve_user(tokens.id_token)
 
-    user = await auth_service.authenticate_user(google_user)
-    jwt = auth_service.build_jwt_from_user(user)
+        user = await auth_service.authenticate_user(google_user)
 
-    return SuccessResponse(
-        message="Google user successfully authenticated", data=LoginResponse(token=jwt)
-    )
+        token = jwt_service.encode(payload=JWTPayload(provider_id=user.provider_id))
+
+        # TODO: Update the redirect url here
+        redirect = RedirectResponse("http://127.0.0.1:8000/redoc")
+        # Set jwt on cookie
+        redirect.set_cookie(key="token", value=token, httponly=True)
+
+        return redirect
+    except jwt.InvalidTokenError:
+        # TODO: Redirect user on fail
+        pass
