318fcf9fbf6fe9e8d0a3667958c974d8b16b7de8
Kurt <secretariokurt3219@gmail.com>
2026-07-20 16:58:13 +0800

    schema: create JWTPayload schema

--- backend/app/schema/auth_schema.py
+++ backend/app/schema/auth_schema.py
@@ -1,19 +1,8 @@
-from datetime import datetime
+import time
+from datetime import datetime, timedelta
 from pydantic import BaseModel, Field, EmailStr, AliasChoices
 
-
-class LoginResponse(BaseModel):
-    token: str
-
-
-class GoogleOAuthTokenResponse(BaseModel):
-    access_token: str
-    refresh_token: str | None = Field(default=None)
-    id_token: str
-    scope: str
-    token_type: str
-    expires_in: datetime
-
+from backend.app.core.settings import settings
 
 class AuthUser(BaseModel):
     provider_id: str = Field(validation_alias=AliasChoices("sub"))
@@ -22,3 +11,29 @@ class AuthUser(BaseModel):
     email: EmailStr
     profile_picture: str = Field(alias="picture")
     email_verified: bool
+
+
+class JWTPayload(BaseModel):
+    provider_id: str
+    iss: str = Field(default=settings.JWT_ISS)
+    aud: str = Field(default=settings.JWT_AUD)
+    iat: int = Field(default_factory=lambda: int(time.time()))
+    exp: int = Field(
+        default_factory=lambda: int(
+            (datetime.now() + timedelta(minutes=settings.JWT_EXP_IN_MIN)).timestamp()
+        )
+    )
+
+
+class LoginResponse(BaseModel):
+    token: str
+    token_type: str
+    
+
+class GoogleOAuthTokenResponse(BaseModel):
+    access_token: str
+    refresh_token: str | None = Field(default=None)
+    id_token: str
+    scope: str
+    token_type: str
+    expires_in: datetime
