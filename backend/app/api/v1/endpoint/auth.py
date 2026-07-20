ae77f7c8e7c9a463baa957dc59b202d11e6c03b3
Kurt <secretariokurt3219@gmail.com>
2026-07-20 15:07:45 +0800

    change: update google callback api to return JWT token

--- backend/app/api/v1/endpoint/auth.py
+++ backend/app/api/v1/endpoint/auth.py
@@ -1,7 +1,13 @@
 from typing import Annotated
 from fastapi import APIRouter, Request, Query, Depends
+
 from backend.app.service.auth_service import AuthService
+from backend.app.service.jwt_service import JWTService
 from backend.app.service.google_service import GoogleService
+
+from backend.app.schema.auth_schema import LoginResponse
+from backend.app.schema.response_schema import SuccessResponse
+
 from backend.app.api.v1.dependency import get_auth_service, get_google_service
 
 auth_api_router = APIRouter()
@@ -18,10 +24,15 @@ def google_login(
 async def google_login_callback(
     code: Annotated[str, Query()],
     auth_service: Annotated[AuthService, Depends(get_auth_service)],
-    google_service: Annotated[GoogleService, Depends(get_google_service)]
-): 
+    google_service: Annotated[GoogleService, Depends(get_google_service)],
+):
     tokens = await google_service.request_tokens(code)
-    
+
     google_user = await google_service.retrieve_user(tokens.id_token)
-    
-    return await auth_service.authenticate_google_user(google_user)
+
+    user = await auth_service.authenticate_google_user(google_user)
+    jwt = auth_service.build_jwt_from_user(user)
+
+    return SuccessResponse(
+        message="Google user successfully authenticated", data=LoginResponse(token=jwt)
+    )
