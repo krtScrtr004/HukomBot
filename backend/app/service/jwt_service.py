aea45d78703ed6e69d1141aaac64ffa8edce59d6
Kurt <secretariokurt3219@gmail.com>
2026-07-20 16:58:53 +0800

    change: modify JWTService's decode() to verify()

--- backend/app/service/jwt_service.py
+++ backend/app/service/jwt_service.py
@@ -1,17 +1,34 @@
 import jwt
 
 from backend.app.core.settings import settings
+from backend.app.schema.auth_schema import JWTPayload
 
 
 class JWTService:
     def __init__(
-        self, secret: str = settings.JWT_SECRET, algo: str = settings.JWT_ALGO
+        self,
+        secret: str = settings.JWT_SECRET,
+        algo: str = settings.JWT_ALGO,
+        iss: str = settings.JWT_ISS,
+        aud: str = settings.JWT_AUD,
     ):
         self._secret = secret
         self._algo = algo
+        self._iss = iss
+        self._aud = aud
 
-    def encode(self, payload: dict):
+    def encode(self, payload: JWTPayload):
         return jwt.encode(payload=payload, key=self._secret, algorithm=self._algo)
 
-    def decode(self, token: str):
-        return jwt.decode(jwt=token, key=self._secret, algorithms=[self._algo])
+    def verify(
+        self,
+        token: str,
+    ):
+        decoded = jwt.decode(jwt=token, key=self._secret, algorithms=[self._algo])
+        payload = JWTPayload.model_validate(decoded["payload"])
+
+        if payload.iss != self._iss:
+            raise jwt.InvalidTokenError("Invalid token issuer")
+
+        if payload.aud != self._aud:
+            raise jwt.InvalidTokenError("Invalid token audience")
