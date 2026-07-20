1c9e33f6a9e4fd3a84407625d17ed3a2e2b948ff
Kurt <secretariokurt3219@gmail.com>
2026-07-20 23:57:43 +0800

    change: add security to oauth flow (state, PKCE, nonce)

--- backend/app/api/v1/endpoint/auth.py
+++ backend/app/api/v1/endpoint/auth.py
@@ -1,4 +1,7 @@
 import jwt
+import base64
+import hashlib
+import secrets
 
 from typing import Annotated
 from urllib.parse import urlencode
@@ -15,6 +18,8 @@ from backend.app.service.google_service import GoogleService
 from backend.app.schema.auth_schema import LoginResponse
 from backend.app.schema.response_schema import SuccessResponse
 
+from backend.app.exception.oauth_exception import OAuthException
+
 from backend.app.api.v1.dependency import (
     get_auth_service,
     get_jwt_service,
@@ -26,23 +31,53 @@ auth_api_router = APIRouter()
 
 @auth_api_router.get("/google/login")
 def google_login(
+    request: Request,
     service: Annotated[GoogleService, Depends(get_google_service)],
 ):
-    return service.redirect_to_authorization()
+    state = secrets.token_urlsafe(32)
+    code_verifier = secrets.token_urlsafe(64)
+    digest = hashlib.sha256(code_verifier.encode()).digest()
+    code_challenge = base64.urlsafe_b64encode(digest).decode().rstrip("=")
+    nonce = secrets.token_urlsafe(32)
+
+    request.session["oauth_state"] = state
+    request.session["oauth_code_verfier"] = code_verifier
+    request.session["oauth_nonce"] = nonce
+
+    return service.redirect_to_authorization(
+        state=state, code_challenge=code_challenge, nonce=nonce
+    )
 
 
 @auth_api_router.get("/google/login/callback")
 async def google_login_callback(
     request: Request,
     code: Annotated[str, Query()],
+    state: Annotated[str, Query()],
     jwt_service: Annotated[JWTService, Depends(get_jwt_service)],
     auth_service: Annotated[AuthService, Depends(get_auth_service)],
     google_service: Annotated[GoogleService, Depends(get_google_service)],
 ):
     try:
-        tokens = await google_service.request_tokens(code)
+        session_state = request.session.get("oauth_state")
+        if not session_state or session_state != state:
+            raise OAuthException(details=["Invalid OAuth state"], status_code=401)
 
-        google_user = await google_service.retrieve_user(tokens.id_token)
+        code_verifier = request.session.get("oauth_code_verfier")
+        if not code_verifier:
+            raise OAuthException(
+                details=["Invalid OAuth code verifier"], status_code=401
+            )
+
+        nonce = request.session.get("oauth_nonce")
+        if not nonce:
+            raise OAuthException(details=["Invalid OAuth nonce"], status_code=401)
+
+        tokens = await google_service.request_tokens(
+            authorization_code=code, code_verifier=code_verifier
+        )
+
+        google_user = await google_service.retrieve_user(tokens.id_token, nonce)
 
         user = await auth_service.authenticate_user(google_user)
 
@@ -55,18 +90,26 @@ async def google_login_callback(
 
         return redirect
     except jwt.ExpiredSignatureError:
-        # TODO: Redirect user on fail
         params = urlencode({"error_code": "TOKEN_EXPIRED"})
         return RedirectResponse(
             url=request.url_for(f"login_page?{params}"), status_code=303
         )
     except (
         jwt.InvalidTokenError,
-        jwt.InvalidAlgorithmError, 
+        jwt.InvalidAlgorithmError,
         jwt.InvalidAudienceError,
-        jwt.InvalidIssuerError
+        jwt.InvalidIssuerError,
     ):
         params = urlencode({"error_code": "INVALID_TOKEN"})
         return RedirectResponse(
             url=request.url_for(f"login_page?{params}"), status_code=303
         )
+    except OAuthException as ex:
+        params = urlencode({"error_code": ex.code})
+        return RedirectResponse(
+            url=request.url_for(f"login_page?{params}"), status_code=303
+        )
+    finally:
+        request.session.pop("oauth_state")
+        request.session.pop("oauth_code_verfier")
+        request.session.pop("oauth_nonce")
--- backend/app/service/google_service.py
+++ backend/app/service/google_service.py
@@ -20,13 +20,33 @@ from backend.app.exception.oauth_exception import (
 
 logger = logging.getLogger(__name__)
 
+
 class GoogleService:
-    def redirect_to_authorization(self):
+    def redirect_to_authorization(self, state: str, code_challenge: str, nonce: str):
+        if not state:
+            raise OAuthException(
+                details=["Invalid state to redirect to Google authentication"]
+            )
+            
+        if not code_challenge:
+            raise OAuthException(
+                details=["Invalid code challenge to redirect to Google authentication"]
+            )
+            
+        if not nonce:
+            raise OAuthException(
+                details=["Invalid nonce to redirect to Google authentication"]
+            )
+
         params = {
             "client_id": settings.OAUTH_CLIENT_ID,
             "redirect_uri": settings.GOOGLE_OAUTH_REDIRECT_URI,
             "response_type": "code",
             "scope": "openid email profile",
+            "state": state,
+            "code_challenge": code_challenge,
+            "code_challenge_method": "S256",
+            "nonce": nonce
         }
 
         try:
@@ -38,41 +58,59 @@ class GoogleService:
                 code="OAUTH_REDIRECT_FAIL",
                 details=[str(ex)],
             )
-            
-    async def request_tokens(self, authorization_code: str):
-        async with AsyncClient() as client:
-            url = settings.GOOGLE_TOKEN_URL
-            body = {
-                "code": authorization_code,
-                "client_id": settings.OAUTH_CLIENT_ID,
-                "client_secret": settings.OAUTH_CLIENT_SECRET,
-                "redirect_uri": settings.GOOGLE_OAUTH_REDIRECT_URI,
-                "grant_type": "authorization_code",
-            }
 
-            response = await client.post(url=url, data=body)
-            if not response or response.status_code != 200:
-                raise OAuthException(
-                    message="Google authentication fail",
-                    code="OAUTH_REQUEST_TOKEN_FAIL",
-                    details=["Failed to retrieve tokens from Google"],
-                )
+    async def request_tokens(self, authorization_code: str, code_verifier: str):
+        if not authorization_code:
+            raise OAuthException(
+                details=["Invalid authorization code on Google login callback"]
+            )
+        
+        if not code_verifier:
+            raise OAuthException(
+                details=["Invalid code verifier on Google login callback"]
+            )
 
-        token_response = GoogleOAuthTokenResponse.model_validate(response.json())
-        return token_response
+        try:
+            async with AsyncClient() as client:
+                url = settings.GOOGLE_TOKEN_URL
+                body = {
+                    "code": authorization_code,
+                    "client_id": settings.OAUTH_CLIENT_ID,
+                    "client_secret": settings.OAUTH_CLIENT_SECRET,
+                    "redirect_uri": settings.GOOGLE_OAUTH_REDIRECT_URI,
+                    "grant_type": "authorization_code",
+                    "code_verifier": code_verifier
+                }
 
-    async def retrieve_user(self, id_token: str):
-        resource = self._decode_id_token(id_token)
+                response = await client.post(url=url, data=body)
+                if not response or response.status_code != 200:
+                    raise OAuthException(
+                        message="Google authentication fail",
+                        code="OAUTH_REQUEST_TOKEN_FAIL",
+                        details=["Failed to retrieve tokens from Google"],
+                    )
+
+            token_response = GoogleOAuthTokenResponse.model_validate(response.json())
+            return token_response
+        except Exception as ex:
+            logger.exception("An error occured while requesting for OAuth tokens: %s", str(ex))
+            raise
+
+    async def retrieve_user(self, id_token: str, session_nonce: str):
+        resource = self._verify_id_token(id_token, session_nonce)
         if not resource.email_verified:
             raise GoogleEmailNotVerifiedException(
                 details=[f"{resource.email} has not been verified by Google"]
             )
-    
+
         return resource
 
-    def _decode_id_token(self, token: str) -> AuthUser:
+    def _verify_id_token(self, token: str, session_nonce: str) -> AuthUser:
         # Verify and decode token
         id_info = id_token.verify_oauth2_token(
             token, requests.Request(), settings.OAUTH_CLIENT_ID
         )
+        nonce = id_info["nonce"]
+        if not nonce or session_nonce != nonce:
+            raise OAuthException(details=["Invalid nonce from Google's ID Token"])
         return AuthUser.model_validate(id_info)
