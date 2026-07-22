ebc31b49d3c3569708fd820c15da26297b6a6637
Kurt <secretariokurt3219@gmail.com>
2026-07-22 20:40:10 +0800

    fix: fix base_to_response on CaseAnalysisVersionCaster didnt return the caseted object

--- backend/app/util/case_analysis_version_caster.py
+++ backend/app/util/case_analysis_version_caster.py
@@ -7,10 +7,10 @@ class CaseAnalysisVersionCaster:
     def base_to_response(
         case_analysis_version: CaseAnalysisVersion, case_facts: list[CaseFact] = []
     ) -> CaseAnalysisVersionResponse:
-        CaseAnalysisVersionResponse(
+        return CaseAnalysisVersionResponse(
             id=case_analysis_version.id,
             version_number=case_analysis_version.version_number,
             answer=case_analysis_version.answer,
             created_at=case_analysis_version.created_at,
             case_facts=case_facts,
-        ),
+        )
