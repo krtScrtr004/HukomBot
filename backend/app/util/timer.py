a381de3cb8517fbcb3d7a3f2918eaead9c3f0eb8
Kurt <secretariokurt3219@gmail.com>
2026-07-22 23:06:55 +0800

    change: modify the timer context manager to be a class

--- backend/app/util/timer.py
+++ backend/app/util/timer.py
@@ -1,11 +1,16 @@
 import time
-from contextlib import contextmanager
 
-@contextmanager
-def timer(label="Process"):
-    start = time.perf_counter()
-    try:
-        yield
-    finally:
-        end = time.perf_counter()
-        return end - start
+class Timer:
+    def __init__(self, label="Process"):
+        self.label = label
+        self.elapsed = 0.0
+        self._start = None
+
+    def __enter__(self):
+        self._start = time.perf_counter()
+        return self  # Binds 'self' to the 'as' variable
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.elapsed = time.perf_counter() - self._start
+        # Returning False allows any internal exceptions to raise normally
+        return False
