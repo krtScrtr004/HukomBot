11d1a3d65d2c8f6d7000c6e57d7f6653b53eb296
Kurt <secretariokurt3219@gmail.com>
2026-08-22 01:28:17 +0800

    change: add get_by_id wrappper and is_valid_image_type methods in UserService

--- backend/hukom_bot/service/user_service.py
+++ backend/hukom_bot/service/user_service.py
@@ -2,9 +2,12 @@ from psycopg import AsyncConnection
 from backend.hukom_bot.database.database import Database
 from backend.hukom_bot.schema.user_schema import UserCreate, UserUpdate
 from backend.hukom_bot.repository.user_repository import UserRepository
+from backend.hukom_bot.util.file_utilities import is_valid_file_type
 
 
 class UserService:
+    ALLOWED_IMAGE_TYPES = {"image/jpg", "image/jpeg", "image/png"}
+
     def __init__(self, db: Database, user_repo: UserRepository):
         self._db = db
         self._user_repo = user_repo
@@ -14,12 +17,26 @@ class UserService:
     async def create(self, user: UserCreate, connection: AsyncConnection = None):
         return await self._user_repo.create(user=user, connection=connection)
 
-    async def update(self, user: UserUpdate, connection: AsyncConnection = None):            
+    async def update(self, user: UserUpdate, connection: AsyncConnection = None):
         await self._user_repo.update(user=user, connection=connection)
+        
+    async def get_by_id(
+        self, id: str, connection: AsyncConnection = None
+    ):
+        return await self._user_repo.get_by_id(
+            id=id, connection=connection
+        )
 
     async def get_by_provider_id(
         self, provider_id: str, connection: AsyncConnection = None
     ):
         return await self._user_repo.get_by_provider_id(
             provider_id=provider_id, connection=connection
+        )
+
+    # Others ===============================
+
+    def is_valid_image_type(self, contents: bytes) -> bool:
+        return is_valid_file_type(
+            content=contents, allowed_file_types=UserService.ALLOWED_IMAGE_TYPES
         )
