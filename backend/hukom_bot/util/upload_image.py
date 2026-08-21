62ef69abcf0cf5a1d0ff2b4d77975d4fc42edbe5
Kurt <secretariokurt3219@gmail.com>
2026-08-22 01:24:44 +0800

    change: add function to remove uploaded image to cloudinary in upload_image module

--- backend/hukom_bot/util/upload_image.py
+++ backend/hukom_bot/util/upload_image.py
@@ -4,18 +4,19 @@ from fastapi import UploadFile
 from backend.hukom_bot.core.settings import settings
 from backend.hukom_bot.schema.util_schema import CloudinaryUploadResponse
 
-
 cloudinary.config(
-    cloud_name = settings.CLOUDINARY_NAME, 
-    api_key = settings.CLOUDINARY_API_KEY, 
-    api_secret = settings.CLOUDINARY_SECRET,
-    secure=True
+    cloud_name=settings.CLOUDINARY_NAME,
+    api_key=settings.CLOUDINARY_API_KEY,
+    api_secret=settings.CLOUDINARY_SECRET,
+    secure=True,
 )
 
-async def upload_to_cloudinary(file: UploadFile) -> CloudinaryUploadResponse:
-    result = cloudinary.uploader.upload(
-        file.file,
-        folder="hukom_bot/images"
-    )
-    
-    return result
+
+def upload_to_cloudinary(file: UploadFile) -> CloudinaryUploadResponse:
+    result = cloudinary.uploader.upload(file.file, folder="hukom_bot/images")
+    return CloudinaryUploadResponse.model_validate(result, extra="ignore")
+
+
+def remove_from_cloudinary(public_id: str) -> bool:
+    result = cloudinary.uploader.destroy(public_id=public_id)
+    return result.get("result") == "ok"
