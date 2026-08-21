83754c1ae1cc17605233d61fbc1204b4f91aab53
Kurt <secretariokurt3219@gmail.com>
2026-08-22 00:19:08 +0800

    change: add type hint on is_valid_file_type()

--- backend/hukom_bot/util/file_utilities.py
+++ backend/hukom_bot/util/file_utilities.py
@@ -1,8 +1,12 @@
 import magic
 
+
 def get_mime_type(content: bytes) -> str:
     return magic.from_buffer(content, mime=True)
 
-def is_valid_file_type(content: bytes, allowed_file_types = []) -> bool:
-    mime =  get_mime_type(content)
+
+def is_valid_file_type(
+    content: bytes, allowed_file_types: list[str] | set[str]
+) -> bool:
+    mime = get_mime_type(content)
     return mime in allowed_file_types
