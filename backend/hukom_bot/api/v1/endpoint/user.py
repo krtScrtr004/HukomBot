aa36008d1d40a562d8e42c8380a21c5baa7a5bbf
Kurt <secretariokurt3219@gmail.com>
2026-08-22 14:58:44 +0800

    change: modify the structure of PATCH endpoint for user endpoint

--- backend/hukom_bot/api/v1/endpoint/user.py
+++ backend/hukom_bot/api/v1/endpoint/user.py
@@ -1,17 +1,18 @@
+from uuid import UUID
 from typing import Annotated
-from fastapi import APIRouter, Depends, Body
+from fastapi import APIRouter, Depends, Path, Form, File, UploadFile
 from backend.hukom_bot.model.user_model import User
 from backend.hukom_bot.schema.user_schema import UserUpdateBase
-from backend.hukom_bot.util.user_caster import UserCaster
-from backend.hukom_bot.service.user_service import UserService
-from backend.hukom_bot.service.token_quota_service import TokenQuotaService
+from backend.hukom_bot.enum.user_role import UserRole
 from backend.hukom_bot.schema.response_schema import SuccessResponse
-
+from backend.hukom_bot.service.token_quota_service import TokenQuotaService
+from backend.hukom_bot.orchistrator.user_orchistrator import UserOrchistrator
+from backend.hukom_bot.util.user_caster import UserCaster
 from backend.hukom_bot.api.v1.dependency import (
     verify_user,
     rate_limit,
-    get_user_service,
     get_token_quota_service,
+    get_user_orchistrator,
 )
 
 user_api_router = APIRouter()
@@ -38,18 +39,29 @@ async def get_daily_token_usage(
     return await service.retrieve_usage(user.id)
 
 
-@user_api_router.patch("/me")
+@user_api_router.patch("/{user_id}")
 async def update_user_info(
-    payload: Annotated[UserUpdateBase, Body],
+    *,
+    user_id: Annotated[UUID, Path()],
+    first_name: Annotated[str | None, Form()] = None,
+    last_name: Annotated[str | None, Form()] = None,
+    role: Annotated[UserRole | None, Form()] = None,
+    profile_picture: Annotated[UploadFile | None, File()] = None,
     user: Annotated[User, Depends(verify_user)],
-    service: Annotated[UserService, Depends(get_user_service)],
+    orchistrator: Annotated[UserOrchistrator, Depends(get_user_orchistrator)],
     _=Depends(rate_limit(limit=10, window=60)),
 ):
-    # Use authenticated user's own id
-    await service.update(
-        user=UserCaster.update_base_to_update(id=user.id, user=payload)
+    payload = UserUpdateBase(
+        first_name=first_name,
+        last_name=last_name,
+        role=role,
     )
 
+    if payload.model_fields_set or profile_picture is not None:
+        await orchistrator.update_pipeline(
+            me=user, user_id=user_id, user=payload, profile_picture=profile_picture
+        )
+
     return SuccessResponse(
-        message="User info successfully updated", data={"id": user.id}
+        message="User info successfully updated", data={"id": user_id}
     )
