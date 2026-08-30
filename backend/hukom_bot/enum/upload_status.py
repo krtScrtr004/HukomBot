7340c4aa20e62cb582a8b47fd583d2509c843b26
Kurt <secretariokurt3219@gmail.com>
2026-08-30 22:35:41 +0800

    change: add 'rejected' value to UploadStatus enum

--- backend/hukom_bot/enum/upload_status.py
+++ backend/hukom_bot/enum/upload_status.py
@@ -1,29 +1,34 @@
 from enum import StrEnum
 
 class UploadStatus(StrEnum):
+    FAILED = "failed"
+    REJECTED = "rejected"
     PENDING = "pending"
     ONGOING = "ongoing"
     COMPLETED = "completed"
-    FAILED = "failed"
     
     def display_name(self) -> str:
         match self:
+            case UploadStatus.FAILED:
+                return "Failed"
+            case UploadStatus.REJECTED:
+                return "Rejected"
             case UploadStatus.PENDING:
                 return "Pending"
             case UploadStatus.ONGOING:
                 return "Ongoing"
             case UploadStatus.COMPLETED:
                 return "Completed"
-            case UploadStatus.FAILED:
-                return "Failed"    
             
     def get_level(self) -> int:
         match self:
+            case UploadStatus.FAILED:
+                return -2
+            case UploadStatus.REJECTED:
+                return -1
             case UploadStatus.PENDING:
                 return 0
             case UploadStatus.ONGOING:
                 return 1
             case UploadStatus.COMPLETED:
                 return 2
-            case UploadStatus.FAILED:
-                return 3    
