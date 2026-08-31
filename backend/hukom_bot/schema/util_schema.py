ccdec2a26f3f15595abc351bb88057eaef1522fc
Kurt <secretariokurt3219@gmail.com>
2026-08-31 23:19:42 +0800

    schema: create utility schema that will hold admin dashboard data

--- backend/hukom_bot/schema/util_schema.py
+++ backend/hukom_bot/schema/util_schema.py
@@ -1,4 +1,15 @@
-from pydantic import BaseModel, HttpUrl
+from pydantic import BaseModel, HttpUrl, Field
+
+
+class AdminDashboardData(BaseModel):
+    active_user_count: int = Field(default=0, min=0)
+    documents_count: int = Field(default=0, min=0)
+    pending_document_count: int = Field(default=0, min=0)
+    ongoing_document_count: int = Field(default=0, min=0)
+    completed_document_count: int = Field(default=0, min=0)
+    failed_document_count: int = Field(default=0, min=0)
+    rejected_document_count: int = Field(default=0, min=0)
+    chunks_count: int = Field(default=0, min=0)
 
 
 class CloudinaryUploadResponse(BaseModel):
