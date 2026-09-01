1704d82f2a636e7661d2b338570b37f859ea581f
Kurt <secretariokurt3219@gmail.com>
2026-09-01 22:04:56 +0800

    fix: fix typos and import order in PubsubService

--- backend/hukom_bot/service/pubsub_service.py
+++ backend/hukom_bot/service/pubsub_service.py
@@ -1,5 +1,6 @@
-import json
 from __future__ import annotations
+
+import json
 from redis.asyncio import Redis
 
 
@@ -12,17 +13,17 @@ class PubsubService:
         await self._pubsub.subscribe(channels)
 
     async def publish(self, channel: str, data: any) -> int:
-        return await self._redis.publish(channel=channel, message=json.dump(data))
+        return await self._redis.publish(channel=channel, message=json.dumps(data))
 
     async def get_message(
         self, ignore_subscribe_messages: bool = False, timeout: float | None = 0
     ) -> str | None:
         return await self._pubsub.get_message(
-            ignore_subscribe_message=ignore_subscribe_messages, timeout=timeout
+            ignore_subscribe_messages=ignore_subscribe_messages, timeout=timeout
         )
 
     async def unsubscribe(self, *channels):
         await self._pubsub.unsubscribe(channels)
 
     async def close(self):
-        await self._pubsub.close
+        await self._pubsub.close()
