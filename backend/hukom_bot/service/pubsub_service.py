352aa02610bdbb8b8f9d3b408edc03b55b8e4572
Kurt <secretariokurt3219@gmail.com>
2026-09-12 00:49:54 +0800

    fix: fix arguments not unpacked on PubsubService methods

--- backend/hukom_bot/service/pubsub_service.py
+++ backend/hukom_bot/service/pubsub_service.py
@@ -8,14 +8,14 @@ from redis.asyncio import Redis
 class PubsubService:
     def __init__(self, redis: Redis):
         self._redis = redis
-        self._pubsub = redis.pubsub()
+        self._pubsub = self._redis.pubsub()
 
     async def subscribe(self, *channels):
-        await self._pubsub.subscribe(channels)
+        await self._pubsub.subscribe(*channels)
 
     async def publish(self, channel: str, data: Any) -> int:
         return await self._redis.publish(channel=channel, message=json.dumps(data))
-
+    
     async def get_message(
         self, ignore_subscribe_messages: bool = False, timeout: float | None = 0
     ) -> dict[str, Any] | None:
@@ -24,7 +24,7 @@ class PubsubService:
         )
 
     async def unsubscribe(self, *channels):
-        await self._pubsub.unsubscribe(channels)
+        await self._pubsub.unsubscribe(*channels)
 
     async def close(self):
         await self._pubsub.close()
