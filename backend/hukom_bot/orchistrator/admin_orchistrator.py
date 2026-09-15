8fdb5050c0cfe87cdc9a998811411d2ad9ffad20
Kurt <secretariokurt3219@gmail.com>
2026-09-15 21:34:12 +0800

    schama: separate schema file for admin features

--- backend/hukom_bot/orchistrator/admin_orchistrator.py
+++ backend/hukom_bot/orchistrator/admin_orchistrator.py
@@ -1,9 +1,14 @@
+from psycopg import AsyncConnection
 from backend.hukom_bot.database.database import Database
 from backend.hukom_bot.service.chunk_service import ChunkService
 from backend.hukom_bot.service.document_service import DocumentService
 from backend.hukom_bot.service.user_service import UserService
+from backend.hukom_bot.schema.admin_schema import (
+    AdminDashboardData,
+    DocumentStatusCount,
+    DocumentWeeklyCount,
+)
 from backend.hukom_bot.schema.mixin import DateRangeableMixin
-from backend.hukom_bot.schema.util_schema import AdminDashboardData
 
 
 class AdminOrchistrator:
@@ -25,45 +30,29 @@ class AdminOrchistrator:
         to_return = AdminDashboardData()
 
         async with self._db.connection() as conn:
-            # User
+            # User ========================================================
             to_return.active_user_count = await self._user_service.count_active(
                 date_range=date_range, connection=conn
             )
 
-            # Documents
+            # Documents ===================================================
+            
+            # All
             to_return.documents_count = await self._document_service.count_all(
                 date_range=date_range, connection=conn
             )
 
-            to_return.pending_document_count = (
-                await self._document_service.count_pending(
-                    date_range=date_range, connection=conn
-                )
-            )
-
-            to_return.ongoing_document_count = (
-                await self._document_service.count_ongoing(
-                    date_range=date_range, connection=conn
-                )
-            )
-
-            to_return.completed_document_count = (
-                await self._document_service.count_completed(
-                    date_range=date_range, connection=conn
-                )
-            )
-
-            to_return.failed_document_count = await self._document_service.count_failed(
+            # By Status
+            to_return.document_status_count = await self._build_document_status_count(
                 date_range=date_range, connection=conn
             )
 
-            to_return.rejected_document_count = (
-                await self._document_service.count_rejected(
-                    date_range=date_range, connection=conn
-                )
+            # Weekly (Mon to Sun)
+            to_return.document_weekly_count = await self._document_service.count_weekly(
+                connection=conn
             )
 
-            # Chunks
+            # Chunks =====================================================
             to_return.chunks_count = await self._chunk_service.count_all(
                 date_range=date_range, connection=conn
             )
@@ -71,3 +60,30 @@ class AdminOrchistrator:
             await conn.commit()
 
             return to_return
+
+    async def _build_document_status_count(
+        self, date_range: DateRangeableMixin, connection: AsyncConnection
+    ) -> DocumentStatusCount:
+        obj = DocumentStatusCount()
+
+        obj.pending = await self._document_service.count_pending(
+            date_range=date_range, connection=connection
+        )
+
+        obj.ongoing = await self._document_service.count_ongoing(
+            date_range=date_range, connection=connection
+        )
+
+        obj.completed = await self._document_service.count_completed(
+            date_range=date_range, connection=connection
+        )
+
+        obj.failed = await self._document_service.count_failed(
+            date_range=date_range, connection=connection
+        )
+
+        obj.rejected = await self._document_service.count_rejected(
+            date_range=date_range, connection=connection
+        )
+
+        return obj
--- backend/hukom_bot/schema/util_schema.py
+++ backend/hukom_bot/schema/util_schema.py
@@ -1,17 +1,6 @@
 from pydantic import BaseModel, HttpUrl, Field
 
 
-class AdminDashboardData(BaseModel):
-    active_user_count: int = Field(default=0, min=0)
-    documents_count: int = Field(default=0, min=0)
-    pending_document_count: int = Field(default=0, min=0)
-    ongoing_document_count: int = Field(default=0, min=0)
-    completed_document_count: int = Field(default=0, min=0)
-    failed_document_count: int = Field(default=0, min=0)
-    rejected_document_count: int = Field(default=0, min=0)
-    chunks_count: int = Field(default=0, min=0)
-
-
 class CloudinaryUploadResponse(BaseModel):
     public_id: str
     secure_url: HttpUrl
