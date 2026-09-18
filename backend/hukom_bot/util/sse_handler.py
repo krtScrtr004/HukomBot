e01dea3bc09ea126cd51af3ee3c54026abec30bf
Kurt <secretariokurt3219@gmail.com>
2026-09-18 22:46:58 +0800

    util: create centralized sse handler function

---
+++ backend/hukom_bot/util/sse_handler.py
@@ -0,0 +1,34 @@
+from fastapi import Request
+from pydantic import BaseModel
+from typing import Callable, Awaitable
+from backend.hukom_bot.service.pubsub_service import PubsubService
+
+
+async def sse_handler(
+    request: Request,
+    channel_name: str,
+    data_builder: Callable[[], Awaitable[BaseModel]],
+    pubsub_service: PubsubService,
+    interval: float = 15.0,
+):
+    await pubsub_service.subscribe(channel_name)
+
+    try:
+        while True:
+            if await request.is_disconnected():
+                break
+
+            message = await pubsub_service.get_message(
+                ignore_subscribe_messages=True, timeout=interval
+            )
+
+            if message is None:
+                yield ": heartbeat\n\n"
+                continue
+
+            data = await data_builder()
+            yield f"message: {message}, data: {data.model_dump_json()}\n\n"
+
+    finally:
+        await pubsub_service.unsubscribe(channel_name)
+        await pubsub_service.close()
